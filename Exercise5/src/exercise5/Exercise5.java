ff362bee144f15e948d897db1be143b087db07d0
ana.montero <ana.montero@106-22.info2>
2024-09-20 14:18:45 +0200

    fwef

---
+++ Exercise5/src/exercise5/Exercise5.java
@@ -0,0 +1,12 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Ana Montero Rodríguez");
+		System.out.println("Calle Tambre Nº 10");
+		System.out.println();
+		System.out.println("+34 622 41 51 03");
+	}
+
+}
